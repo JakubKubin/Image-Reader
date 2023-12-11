138805ca7d9c11d6d6bdbd1fd4b526941172ab3f
niewinN <109554022+niewinN@users.noreply.github.com>
2023-12-11 15:22:05 +0100

    added main

--- main.py
+++ main.py
@@ -10,22 +10,32 @@ pytesseract.pytesseract.tesseract_cmd = path.TESSERACT_PATH
 os.chdir(path.OUTPUT_PATH)
 
 LANGUAGE = 'pol'
-PATH_TO_IMAGE = path.IMAGE_PATH + r'\zdj.jpg'
 SAVE_PATH = path.OUTPUT_PATH
 
 if __name__ == '__main__':
     startTime = time.process_time()
 
-    image = im.get_image(PATH_TO_IMAGE)
+    image = im.get_image(path.IMAGE_PATH + r'\zdj.jpg')
     image_ROI = im.get_image(path.IMAGE_PATH + r'\zdj_ROI.jpg')
     image_TNR = im.get_image(path.IMAGE_PATH + r'\zdj_TNR.jpg')
     image_Calibri = im.get_image(path.IMAGE_PATH + r'\zdj_Calibri.jpg')
     image_Arial = im.get_image(path.IMAGE_PATH + r'\zdj_Arial.jpg')
 
-    im.image_to_text_file(image, LANGUAGE, SAVE_PATH, "original")
-    im.image_to_text_file(image_ROI, LANGUAGE, SAVE_PATH, "ROI")
-    im.image_to_text_file(image_TNR, LANGUAGE, SAVE_PATH, "TimesNewRoman")
-    im.image_to_text_file(image_Calibri, LANGUAGE, SAVE_PATH, "Calibri")
-    im.image_to_text_file(image_Arial, LANGUAGE, SAVE_PATH, "Arial")
+    #im.image_to_text_file(image, LANGUAGE, SAVE_PATH, "original")
+    #im.image_to_text_file(image_ROI, LANGUAGE, SAVE_PATH, "ROI")
+    #im.image_to_text_file(image_TNR, LANGUAGE, SAVE_PATH, "TimesNewRoman")
+    #im.image_to_text_file(image_Calibri, LANGUAGE, SAVE_PATH, "Calibri")
+    #im.image_to_text_file(image_Arial, LANGUAGE, SAVE_PATH, "Arial")
 
+    image_mean_masked = pp.mean_mask(image)
+    im.image_to_text_file(image_mean_masked, LANGUAGE, SAVE_PATH, "mean_masked")
+    im.save_image(SAVE_PATH + r'\out_original_mean.png', image_mean_masked)
+
+    image_static_masked = pp.static_mask(image_Arial)
+    im.image_to_text_file(image_static_masked, LANGUAGE, SAVE_PATH, "static_image_Arial")
+    im.save_image(SAVE_PATH + r'\out_static_image_Arial.png', image_static_masked)
+
+    image_gaussian_masked = pp.gaussian_mask(image_Arial)
+    im.image_to_text_file(image_gaussian_masked, LANGUAGE, SAVE_PATH, "gaussian_masked_image_Arial")
+    im.save_image(SAVE_PATH + r'\out_gaussian_masked_image_Arial.png', image_gaussian_masked)
     print("Time elapsed:", time.process_time() - startTime, "s")
