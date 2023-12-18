b01dba715e6548bd2f310dd6bfca72f350048cb0
Aniuula <acelinska372@gmail.com>
2023-12-18 13:06:36 +0100

    Added scan photo ghotic and abadi fonts

--- README.md
+++ README.md
@@ -1,45 +1,49 @@
 # Recognizing text using the Tesseract library
 
 ## How to make things work:
+
 - Use Python 3.9.12
-- ```pip install --no-dependencies -r .\requirements.txt```
-- Install Tesseract and Poppler
+- `pip install --no-dependencies -r .\requirements.txt`
+- Install Tesseract
 
 ## How to install Tesseract OCR for Windows
+
 - Download installer [Tesseract-OCR](https://digi.bib.uni-mannheim.de/tesseract/tesseract-ocr-w64-setup-5.3.3.20231005.exe)
 - Install Tesseract for all users
 - Select **ALL** components to be installed
 - Use default destination folder - `C:\Program Files\Tesseract-OCR`
 
 ## Throubleshooting
+
 Possible issues are with incompatible packages, try using 'virtualenv' instead of default pip folder. Make sure that you use proper Python version.
 
 ## Pip list of working packages
-- base256            1.0.1
-- certifi            2022.12.7
+
+- base256 1.0.1
+- certifi 2022.12.7
 - charset-normalizer 3.1.0
-- click              8.1.3
-- colorama           0.4.6
-- emit               0.4.0
-- Flask              2.2.3
-- idna               3.4
-- itsdangerous       2.1.2
-- Jinja2             3.1.2
-- MarkupSafe         2.1.2
-- numpy              1.24.2
-- opencv-python      4.8.1.78
-- packaging          23.0
-- pandas             1.5.3
-- pdf2image          1.16.3
-- Pillow             9.4.0
-- prompt-toolkit     3.0.38
-- pytesseract        0.3.10
-- python-dateutil    2.8.2
-- pytz               2022.7.1
-- requests           2.28.2
-- setuptools         67.1.0
-- six                1.16.0
-- urllib3            1.26.15
-- wcwidth            0.2.6
-- Werkzeug           2.2.3
-- wheel              0.38.4
+- click 8.1.3
+- colorama 0.4.6
+- emit 0.4.0
+- Flask 2.2.3
+- idna 3.4
+- itsdangerous 2.1.2
+- Jinja2 3.1.2
+- MarkupSafe 2.1.2
+- numpy 1.24.2
+- opencv-python 4.8.1.78
+- packaging 23.0
+- pandas 1.5.3
+- pdf2image 1.16.3
+- Pillow 9.4.0
+- prompt-toolkit 3.0.38
+- pytesseract 0.3.10
+- python-dateutil 2.8.2
+- pytz 2022.7.1
+- requests 2.28.2
+- setuptools 67.1.0
+- six 1.16.0
+- urllib3 1.26.15
+- wcwidth 0.2.6
+- Werkzeug 2.2.3
+- wheel 0.38.4
--- main.py
+++ main.py
@@ -29,30 +29,70 @@ if __name__ == '__main__':
     image_TNR = im.get_image(path.IMAGE_PATH + r'\zdj_TNR.jpg')
     image_Calibri = im.get_image(path.IMAGE_PATH + r'\zdj_Calibri.jpg')
     image_Arial = im.get_image(path.IMAGE_PATH + r'\zdj_Arial.jpg')
+    image_Arial = im.get_image(path.IMAGE_PATH + r'\zdj_Arial.jpg')
+    
+    
+    #skan +inne czcionki
+    skan = im.get_image(path.IMAGE_PATH + r'\skan.jpg')
+    skan_ROI = im.get_image(path.IMAGE_PATH + r'\skan_ROI.jpg')
+    skan_Abadi = im.get_image(path.IMAGE_PATH + r'\skan_Abadi.jpg')
+    skan_Ghotic = im.get_image(path.IMAGE_PATH + r'\skan_Ghotic.jpg')
 
     #im.image_to_text_file(image, LANGUAGE, SAVE_PATH, "original")
     #im.image_to_text_file(image_ROI, LANGUAGE, SAVE_PATH, "ROI")
     #im.image_to_text_file(image_TNR, LANGUAGE, SAVE_PATH, "TimesNewRoman")
     #im.image_to_text_file(image_Calibri, LANGUAGE, SAVE_PATH, "Calibri")
     #im.image_to_text_file(image_Arial, LANGUAGE, SAVE_PATH, "Arial")
+    
+    #im.image_to_text_file(skan, LANGUAGE, SAVE_PATH, "original_skan")
+    #im.image_to_text_file(skan_ROI, LANGUAGE, SAVE_PATH, "skan_ROI")
+    #im.image_to_text_file(skan_Abadi, LANGUAGE, SAVE_PATH, "Abadi")
+    #im.image_to_text_file(skan_Ghotic, LANGUAGE, SAVE_PATH, "Ghotic")
+    
 
     original_mean = apply_mask_and_save(image, pp.mean_mask, "mean_masked")
     original_static = apply_mask_and_save(image, pp.static_mask, "static_mask")
+    
+    #skan +inne czcionki
+    skan_original_mean = apply_mask_and_save(skan, pp.mean_mask, "skan_mean_masked")
+    skan_original_static = apply_mask_and_save(skan, pp.static_mask, "skan_static_mask")
 
     original_avg = pp.add_and_average(original_mean, original_static)
     apply_mask_and_save(original_avg, None, "avg_mean_static_original")
-
+    
+    #skan +inne czcionki
+    skan_original_avg = pp.add_and_average(skan_original_mean, skan_original_static)
+    apply_mask_and_save(skan_original_avg, None, "avg_mean_static_original_skan")
+    
+    
+    
     #opened_original_mean = pp.open_binary_image(original_mean)
     opened_original_static = pp.open_binary_image(original_static)
+    
+    #skan +inne czcionki
+    opened_original_static_skan = pp.open_binary_image(skan_original_static)
 
     original_avg_closed = pp.add_and_average(opened_original_static, original_mean)
     apply_mask_and_save(original_avg_closed, None, "avg_opened_mean_static_original")
+    
+    #skan +inne czcionki
+    skan_original_avg_closed = pp.add_and_average(opened_original_static_skan, skan_original_mean)
+    apply_mask_and_save(skan_original_avg_closed, None, "avg_opened_mean_static_original_skan")
 
     mean = apply_mask_and_save(image_Arial, pp.mean_mask, "mean_masked_Arial")
     static = apply_mask_and_save(image_Arial, pp.static_mask, "static_image_Arial")
 
+    #skan +inne czcionki
+    mean_skan = apply_mask_and_save(skan_Abadi, pp.mean_mask, "mean_masked_skan_Abadi")
+    static_skan = apply_mask_and_save(skan_Abadi, pp.static_mask, "static_skan_Abadi")
+    
     average = pp.add_and_average(mean, static)
+    
+    #skan +inne czcionki
+    average_skan = pp.add_and_average(mean_skan, static_skan)
 
     apply_mask_and_save(average, None, "average_mean_static_image_Arial")
+    #skan +inne czcionki
+    apply_mask_and_save(average_skan, None, "average_mean_static_image_Abadi")
 
     print("Time elapsed:", time.process_time() - startTime, "s")
